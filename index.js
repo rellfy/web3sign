4f8907fb302e847fde95c52bdce45a12d6b34d06
Lorenzo Fontoura <lorenzo@nioel.com>
2021-08-27 04:01:00 +1000

    add server package

---
+++ index.js
@@ -0,0 +1,6 @@
+﻿const express = require("express");
+const app = express();
+app.use(express.static("./public"));
+const port = process.env.PORT || 3000;
+app.listen(port);
+console.log(`server listening on port ${port}`);
